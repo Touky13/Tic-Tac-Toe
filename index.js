0d6a5aaa7eb414ef433130f2d9c9834aa73ecab2
Guillaume <197660339+Touky13@users.noreply.github.com>
2025-06-18 16:44:22 +0200

    Added game logic This is extremely dirty

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const gameBoard = (function () {
-    const arrayBot = [1,1,1];
+    const arrayBot = [0,0,0];
     const arrayMid = [0,0,0];
     const arrayTop = [0,0,0];
     return { arrayBot, arrayMid, arrayTop };
@@ -22,3 +22,37 @@ console.log ({
     marker: playerTwo.marker
 });
 
+const gameLogic = (function () {
+    const [ zerothEleBot, firstEleBot, secondEleBot ] = gameBoard.arrayBot;
+    console.log (zerothEleBot, firstEleBot, secondEleBot);
+    const [ zerothEleMid, firstEleMid, secondEleMid ] = gameBoard.arrayMid;
+    console.log (zerothEleMid, firstEleMid, secondEleMid);
+    const [ zerothEleTop, firstEleTop, secondEleTop ] = gameBoard.arrayTop;
+    console.log (zerothEleTop, firstEleTop, secondEleTop);
+
+    const calculator = (function () {
+        const sumBot = zerothEleBot + firstEleBot + secondEleBot;
+        const sumMid = zerothEleMid + firstEleMid + secondEleMid;
+        const sumTop = zerothEleTop + firstEleTop + secondEleTop;
+        const sumRow0 = zerothEleBot + zerothEleMid + zerothEleTop;
+        const sumRow1 = firstEleBot + firstEleMid + firstEleTop;
+        const sumRow2 = secondEleBot + secondEleMid + secondEleTop;
+        const sumDiag0 = zerothEleBot + firstEleMid + secondEleTop;
+        const sumDiag1 = secondEleBot + firstEleMid + zerothEleTop;
+        console.log (sumBot, sumMid, sumTop, sumRow0, sumRow1, sumRow2, sumDiag0, sumDiag1);
+        return { sumBot, sumMid, sumTop, sumRow0, sumRow1, sumRow2, sumDiag0, sumDiag1 };
+    })();
+    console.log (calculator.sumBot, calculator.sumMid, calculator.sumTop);
+
+    if (calculator.sumBot === 3 || calculator.sumBot === -3 || calculator.sumMid === 3 || calculator.sumMid === -3 || calculator.sumTop === 3 || calculator.sumTop === -3) {
+        alert("You win");
+    } else if (calculator.sumRow0 === 3 || calculator.sumRow0 === -3 || calculator.sumRow1 === 3 || calculator.sumRow1 === -3 || calculator.sumRow2 === 3 || calculator.sumRow2 === -3) {
+        alert("You win row");
+    } else if (calculator.sumDiag0 === 3 || calculator.sumDiag0 === -3 || calculator.sumDiag1 === 3 || calculator.sumDiag1 === -3) {
+        alert("You win diag");
+    } else if (gameBoard.arrayBot.includes(0) === true || gameBoard.arrayMid.includes(0) === true || gameBoard.arrayTop.includes(0) === true) {
+        alert("The game continue");
+    } else {
+        alert("It's a tie");
+    }
+})();
