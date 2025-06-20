8f2ed3dd58d9fe3cdc64a0a3cac6948c297562df
Guillaume <197660339+Touky13@users.noreply.github.com>
2025-06-20 21:42:13 +0200

    Done

--- index.js
+++ index.js
@@ -2,45 +2,47 @@ const gameBoard = (function () {
     const array = ['','','','','','','','',''];
     return { array };
 })();
-console.log (gameBoard);
 
 function createPlayer (name, marker) {
-    const userInput = prompt("Please enter your name:")
+    const userInput = prompt("Please enter your name :")
     if (userInput === "") {
         alert("You didn't choose a name.");
         userName = name;
     } else if (userInput !== null) {
-        alert("Hello, " + userInput + "!");
+        alert("Hello, " + userInput + " !");
         userName = userInput;
     } else {
         alert("You canceled the input.");
         userName = name;
     }
-    const body = document.querySelector("body");
-
     return { name, marker, userName };
 };
 
 const playerOne = createPlayer ("player one", "X");
-console.log ({
-    name: playerOne.name,
-    marker: playerOne.marker
-});
-
 const playerTwo = createPlayer ("player two", "O");
-console.log ({
-    name: playerTwo.name,
-    marker: playerTwo.marker
-});
+
 
 const display = (function () {
     const body = document.querySelector("body");
+    const title = document.createElement("h1");
+    title.innerText = "Play a game of Tic-Tac-Toe";
+    body.appendChild(title);
     const div = document.createElement("div");
     body.appendChild(div);
     div.classList.add("gameDisplay");
 
+    gameBoard.array.forEach(item => {
+        let square = document.createElement("span");
+        let p = document.createElement("p");
+        p.innerText = item;
+        div.appendChild(square);
+        square.appendChild(p);
+        square.setAttribute ("data-squareId", document.querySelectorAll("span").length-1);
+    });
+
     const playerInterface = document.createElement("div");
     playerInterface.classList.add("playerInterface");
+
     const player1 = document.createElement("div");
     player1.classList.add("player");
     player1.setAttribute("id", playerOne.name);
@@ -65,14 +67,13 @@ const display = (function () {
     player2.appendChild(p1_2);
     player2.appendChild(p2_2);
 
-    gameBoard.array.forEach(item => {
-        let square = document.createElement("span");
-        let p = document.createElement("p");
-        p.innerText = item;
-        div.appendChild(square);
-        square.appendChild(p);
-        square.setAttribute ("data-squareId", document.querySelectorAll("span").length-1);
-    });
+    const buttons = document.createElement("div");
+    buttons.classList.add("buttons");
+    const resetBtn = document.createElement("button");
+    resetBtn.classList.add("reset");
+    resetBtn.innerText = "Reset";
+    body.appendChild(buttons);
+    buttons.appendChild(resetBtn);
  })();
 
 const gameLogic = (function () {
@@ -81,11 +82,11 @@ const gameLogic = (function () {
         return { checkWin };
     })();
 
-    const test = document.querySelectorAll("span");
+    const gameInit = document.querySelectorAll("span");
     let activePlayer = 0;
     let gameEnd = 0;
 
-    test.forEach(item => {
+    gameInit.forEach(item => {
         item.addEventListener("click", (e) => {
             let target = e.target;
             function validSquare () {
@@ -93,56 +94,64 @@ const gameLogic = (function () {
                     const reset = confirm("The game is over. Do you want to reset the board ?");
                     if (reset === true) {
                         gameBoard.array.splice(0, 9, '','','','','','','','','',);
-                        console.log(gameBoard);
-                        test.forEach(item => {
+                        gameInit.forEach(item => {
                             item.querySelector("p").innerText = '';
                         });
                         gameEnd = 0;
                         activePlayer = 0;
                     }
                 } else if (item.querySelector("p").innerText === playerOne.marker || item.querySelector("p").innerText === playerTwo.marker) {
-                    alert ("Please choose an empty square");
                 } else {
                     activePlayer++;
                     activePlayer = activePlayer % 2;
-                    console.log(activePlayer);
                     switch (activePlayer) {
                         case 1:
                             item.querySelector("p").innerText = playerOne.marker;
-                            console.log(target);
-                            console.log(target.dataset.squareid);
                             gameBoard.array.splice(target.dataset.squareid, 1, 1);
-                            console.log(gameBoard);
                             break;
                         case 0:
                             item.querySelector("p").innerText = playerTwo.marker;
-                            console.log(target);
-                            console.log(target.dataset.squareid);
                             gameBoard.array.splice(target.dataset.squareid, 1, -1);
-                            console.log(gameBoard);
                             break;
                     }
                 }
             }
-            validSquare();                
+            validSquare();
+            
+            function checkWinner() {
+                if (activePlayer % 2 === 1) {
+                    alert(playerOne.userName + " win !");
+                    gameEnd = 1;
+                } else {
+                    alert(playerTwo.userName + " win !");
+                    gameEnd = 1;
+                }
+             }
 
             const [ zerothEle, firstEle, secondEle, thirdEle, fourthEle, fifthEle, sixthEle, seventhEle, eighthEle ] = gameBoard.array;
-            console.log (zerothEle, firstEle, secondEle, thirdEle, fourthEle, fifthEle, sixthEle, seventhEle, eighthEle);
             if (calculator.checkWin(zerothEle, firstEle, secondEle) === 3 || calculator.checkWin(zerothEle, firstEle, secondEle) === -3 || calculator.checkWin(thirdEle, fourthEle, fifthEle) === 3 || calculator.checkWin(thirdEle, fourthEle, fifthEle) === -3 || calculator.checkWin(sixthEle, seventhEle, eighthEle) === 3 || calculator.checkWin(sixthEle, seventhEle, eighthEle) === -3) {
-                alert("You win");
-                gameEnd = 1;
+                checkWinner();
             } else if (calculator.checkWin(zerothEle, thirdEle, sixthEle) === 3 || calculator.checkWin(zerothEle, thirdEle, sixthEle) === -3 || calculator.checkWin(firstEle, fourthEle, seventhEle) === 3 || calculator.checkWin(firstEle, fourthEle, seventhEle) === -3 || calculator.checkWin(secondEle, fifthEle, eighthEle) === 3 || calculator.checkWin(secondEle, fifthEle, eighthEle) === -3) {
-                alert("You win row");
-                gameEnd = 1;
+                checkWinner();
             } else if (calculator.checkWin(zerothEle, fourthEle, eighthEle) === 3 || calculator.checkWin(zerothEle, fourthEle, eighthEle) === -3 || calculator.checkWin(secondEle, fourthEle, sixthEle) === 3 || calculator.checkWin(secondEle, fourthEle, sixthEle) === -3) {
-                alert("You win diag");
-                gameEnd = 1;
+                checkWinner();
             } else if (gameBoard.array.includes('') === true || gameBoard.array.includes('') === true || gameBoard.array.includes('') === true) {
                 //alert("The game continue");
             } else {
-                alert("It's a tie");
+                alert("It's a tie !");
                 gameEnd = 1;
             }
         });
     });
+
+    const buttons = document.querySelector("button");
+    buttons.addEventListener("click", () => {
+        gameBoard.array.splice(0, 9, '','','','','','','','','',);
+        const gameInit = document.querySelectorAll("span");
+        gameInit.forEach(item => {
+            item.querySelector("p").innerText = '';
+        });
+        gameEnd = 0;
+        activePlayer = 0;
+    })
 })();
